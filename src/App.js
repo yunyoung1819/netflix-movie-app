27356582b9755e4eddd26f8bcb539d59e28123ac
yunyoung1819 <yunyoung1819@hanmail.net>
2021-02-27 19:17:16 +0900

    클론코딩 영화 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+function Food(props) {
+    console.log(props);
+    return <h1> I like Potato2 </h1>
+}
+
 function App() {
   return (
       <div>
         <h1>Hello!! 클론코딩 </h1>
+          <Food fav="kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
       </div>
   );
 }
